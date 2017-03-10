f1cd48b17eda35a7159d33310c88f741f0bcd821
Erick Belfort <erick.belf@gmail.com>
2017-03-09 21:11:44 -0500

    test(utils): add more tests to cover whole boilerplate

--- server/tests/utils.test.js
+++ server/tests/utils.test.js
@@ -19,6 +19,33 @@ describe('## Misc', () => {
     });
   });
 
+  describe('# Error Handling', () => {
+    it('Should return mongoose CastError', (done) => {
+      request(app)
+        .get('/api/users/l3k123ljlk123')
+        .expect(httpStatus.INTERNAL_SERVER_ERROR)
+        .then((res) => {
+          expect(res.body.message).to.equal('Internal Server Error');
+          done();
+        })
+        .catch(done);
+    });
+
+    it('Should handle express validation error - username is required', (done) => {
+      request(app)
+        .post('/api/users')
+        .send({
+          mobileNumber: '1234567890'
+        })
+        .expect(httpStatus.BAD_REQUEST)
+        .then((res) => {
+          expect(res.body.message).to.equal('"username" is required');
+          done();
+        })
+        .catch(done);
+    });
+  });
+
   describe('# GET /api/404', () => {
     it('Should return 404 stats', (done) => {
       request(app)
